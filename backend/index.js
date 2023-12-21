f729bd2dc8b69a5ba97b0cceaa0447a7cfc87119
Joshua Okorie <joshuaokorie008@gmail.com>
2023-12-21 14:01:58 +0100

    connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO).then(()=>console.log("connected to database")).catch((error)=>console.log(error))
 const app = express();
 
 app.listen(3000, () => {
